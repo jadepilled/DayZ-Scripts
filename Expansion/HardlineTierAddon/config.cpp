c2e7a858f4dd2be8c05967d0fb7b8244a0eb6749
jade <125251289+jadepilled@users.noreply.github.com>
2025-12-28 20:38:57 +1100

    Fix tier color normalization safety

---
+++ Expansion/HardlineTierAddon/config.cpp
@@ -0,0 +1,38 @@
+class CfgPatches
+{
+    class DZ_Expansion_Hardline_TierAddon
+    {
+        requiredAddons[] = {"DZ_Expansion_Hardline"};
+        units[] = {};
+        weapons[] = {};
+    };
+};
+
+class CfgMods
+{
+    class Expansion_Hardline_TierAddon
+    {
+        dir = "Expansion_Hardline_TierAddon";
+        name = "Expansion Hardline Tier Addon";
+        author = "ServerAdmin";
+        dependencies[] = {"Game", "World", "Mission"};
+
+        class defs
+        {
+            class gameScriptModule
+            {
+                files[] = {"HardlineTierAddon/scripts/3_Game"};
+            };
+
+            class worldScriptModule
+            {
+                files[] = {"HardlineTierAddon/scripts/4_World"};
+            };
+
+            class missionScriptModule
+            {
+                files[] = {"HardlineTierAddon/scripts/5_Mission"};
+            };
+        };
+    };
+};
