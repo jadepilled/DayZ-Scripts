0b5c8f2831d2b3cab34137fc63d6c13df457e473
jade <125251289+jadepilled@users.noreply.github.com>
2025-12-31 03:33:03 +1100

    Add Expansion Hardline compatibility for ItemRarity

--- ItemRarity/scripts/5_mission/inspectmenunew.c
+++ ItemRarity/scripts/5_mission/inspectmenunew.c
@@ -4,34 +4,64 @@ modded class InspectMenuNew
     TextWidget w_RarityText;
 
     override Widget Init()
-	{
-        Widget rootWidget = super.Init();
+    {
+        Widget rootWidget;
+
+        #ifdef EXPANSIONMODHARDLINE
+        if (Expansion_UseCustomLayout())
+        {
+            layoutRoot = g_Game.GetWorkspace().CreateWidgets(Expansion_GetInspectionMenuLayout());
+            rootWidget = layoutRoot;
+        }
+        else
+        {
+            rootWidget = super.Init();
+        }
+        #else
+        rootWidget = super.Init();
+        #endif
+
         Widget w_InventoryInfoPanel = rootWidget.FindAnyWidget("InventoryInfoPanelWidget");
         Widget w_LiquidType = rootWidget.FindAnyWidget("ItemLiquidTypeWidgetBackground");
         Widget w_ItemDesc = rootWidget.FindAnyWidget("ItemDescWidget");
-		
+        Widget w_ExistingRarity = rootWidget.FindAnyWidget("ItemRarityWidgetBackground");
+
         w_RarityPanel = GetGame().GetWorkspace().CreateWidgets("ItemRarity/gui/layouts/inspect_item.layout", w_InventoryInfoPanel);
-        w_RarityText = TextWidget.Cast(w_RarityPanel.FindAnyWidget("ItemRarityWidget"));
+        w_RarityText = TextWidget.Cast(w_RarityPanel.FindAnyWidget("ItemRarityModWidget"));
 
         float x, y;
-        w_LiquidType.GetPos(x, y);
-        w_RarityPanel.SetPos(x, y);
 
-        w_LiquidType.SetPos(x, y + 0.12);
-        
-        w_ItemDesc.GetPos(x, y);
-        w_ItemDesc.SetPos(x, y + 0.04);
+        if (w_ExistingRarity)
+        {
+            w_ExistingRarity.GetPos(x, y);
+            w_RarityPanel.SetPos(x, y + 0.04);
+        }
+        else if (w_LiquidType)
+        {
+            w_LiquidType.GetPos(x, y);
+            w_RarityPanel.SetPos(x, y);
 
-        w_ItemDesc.GetSize(x, y);
-        w_ItemDesc.SetSize(x, y - 0.04);
+            w_LiquidType.SetPos(x, y + 0.12);
+
+            w_ItemDesc.GetPos(x, y);
+            w_ItemDesc.SetPos(x, y + 0.04);
+
+            w_ItemDesc.GetSize(x, y);
+            w_ItemDesc.SetSize(x, y - 0.04);
+        }
 
         return rootWidget;
-	}
+    }
 
     override void SetItem(EntityAI item)
     {
+        #ifdef EXPANSIONMODHARDLINE
+        if (Expansion_UseCustomLayout())
+            InspectMenuNew.Expansion_UpdateItemInfoRarity(layoutRoot, item);
+        #endif
+
         super.SetItem(item);
-    
+
         string rarity = GetRarityConfig().GetRarity(item);
         int rarityColor = GetRarityConfig().GetRarityColor(rarity);
 
